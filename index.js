3e80bbe8e15bfd1ce91a8dc089bf1b3356aa0728
yskoht <ysk.oht@gmail.com>
2018-05-05 23:36:13 +0900

    Add display a draw message

--- index.js
+++ index.js
@@ -113,7 +113,11 @@ class Game extends React.Component {
     if(winner) {
       status = 'Winner: ' + winner;
     } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      if(this.state.stepNumber === 9) {
+        status = 'Draw';
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
     }
 
     return (
